a13d1b8f7dbf5bdc2e1ce3c39eda8a881fc84a41
Mehmet Mücahit ÇALI <mucahit8cali@gmail.com>
2017-03-11 13:25:51 +0300

    Copy Folder
    Folder and sub folders

---
+++ Dizin Kopyalama/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Dizin_Kopyalama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string path1,path2;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+
+        }
+        
+    //Source Directory
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            path1=folderBrowserDialog1.SelectedPath;
+            textBox1.Text = path1;
+            string[] array1 = Directory.GetFiles(textBox1.Text);
+            
+        }
+        //Target Directory
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            path2= folderBrowserDialog1.SelectedPath;
+            textBox2.Text = path2;
+        }
+        //Copy Process
+        private void button3_Click(object sender, EventArgs e)
+        {         
+            Copy(path1,path2);
+            MessageBox.Show("Lets Go!!!");
+        }
+        public static void Copy(string sourceDirectory, string targetDirectory)
+        {
+            DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+            DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+            
+            CopyAll(diSource, diTarget);
+        }
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+    }
+}
